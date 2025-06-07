c725d70335d592d2b2cb289c6f9b144b45f2bc46
Sean <seanturner06@proton.me>
2025-06-07 16:23:03 -0400

    Added icon and adjust limiting for testing.

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 const limiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    limit: 5, // each IP can make up to 5 requests per `windowsMs` (60 minutes)
+    limit: 10, // each IP can make up to 5 requests per `windowsMs` (60 minutes)
     standardHeaders: true, // add the `RateLimit-*` headers to the response
     legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
     message: {
